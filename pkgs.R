60b89e344f24e02fc0fb3599996606a86142f999
vnijs <radiant@rady.ucsd.edu>
2016-07-20 16:17:13 -0700

    cleanup

--- pkgs.R
+++
@@ -1,4 +0,0 @@
-pkgs <- c("car", "miniCRAN", "gridExtra", "GPArotation", "psych", "wordcloud",
-          "AlgDesign", "knitr", "ggdendro", "lubridate", "ggplot2", "pryr",
-          "shiny", "magrittr", "tidyr", "dplyr", "broom", "shiny", "rmarkdown",
-          "shiny","NeuralNetTools", "data.tree", "nnet", "DT")
